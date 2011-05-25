bbb6f09ccdfcd36c050b477140c135ec27694f73
akimatter <akm2000@gmail.com>
2011-05-25 11:48:56 +0900

    [alpha1で必要なパッケージのgemとmm_server_riを作る] rails g cucumber:feature でネームスペースがフィーチャ名などに反映されるようにgeneratorを修正しました

--- lib/generators/cucumber/feature/feature_generator.rb
+++ lib/generators/cucumber/feature/feature_generator.rb
@@ -7,20 +7,28 @@ module Cucumber
     argument :fields, :optional => true, :type => :array, :banner => "[field:type, field:type]"
 
     attr_reader :named_args
-  
+
+    def underscore_singular_name
+      @underscore_singular_name ||= class_name.underscore
+    end
+
+    def underscore_plural_name
+      @underscore_plural_name ||= underscore_singular_name.pluralize
+    end
+
     def parse_fields
       @named_args = @fields.nil? ? [] : @fields.map { |arg| NamedArg.new(arg) }
-    end    
+    end
 
     def generate
       empty_directory 'features/step_definitions'
-      template 'feature.erb', "features/manage_#{plural_name}.feature"
-      template 'steps.erb', "features/step_definitions/#{singular_name}_steps.rb"
+      template 'feature.erb', "features/manage_#{plural_table_name}.feature"
+      template 'steps.erb', "features/step_definitions/#{singular_table_name}_steps.rb"
       gsub_file 'features/support/paths.rb', /'\/'/mi do |match|
-        "#{match}\n    when /the new #{singular_name} page/\n      new_#{singular_name}_path\n"
+        "#{match}\n    when %r{the new #{class_name.underscore.gsub(/\//, "[/_ ]")} page}\n      new_#{singular_table_name}_path\n"
       end
     end
-  
+
     def self.banner
       "#{$0} cucumber:feature ModelName [field:type, field:type]"
     end
